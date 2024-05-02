c61e802bc8ba4281c6c55fbd5146c0086094ccd9
aladadev <aladadev29@gmail.com>
2024-05-02 21:45:49 +0600

    theme done boom

---
+++ food_fete/lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Color.fromARGB(255, 20, 20, 20),
+    primary: const Color.fromARGB(255, 122, 122, 122),
+    secondary: Color.fromARGB(255, 12, 12, 12),
+    tertiary: Color.fromARGB(255, 46, 44, 44),
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
