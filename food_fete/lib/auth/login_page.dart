3237f144f4f12923e1354b718967cf631fa8040e
aladadev <aladadev29@gmail.com>
2024-05-03 10:16:59 +0600

    loginPage

--- food_fete/lib/auth/login_page.dart
+++ food_fete/lib/auth/login_page.dart
@@ -1,10 +1,104 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import 'package:food_fete/components/buttons.dart';
+import 'package:food_fete/components/textfield.dart';
+
+class LoginPage extends StatefulWidget {
+  final VoidCallback? registerFunc;
+
+  const LoginPage({super.key, required this.registerFunc});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+
+            //slogan for the app
+            Text(
+              'Food Fete - Your Food Companion',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+
+            MyTextField(
+                controller: emailController,
+                hintText: 'Email',
+                obscureText: false),
+
+            SizedBox(
+              height: 8,
+            ),
+
+            MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            CustomButton(
+              text: 'Login',
+              onTap: () {},
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            //Not a Member? Navigate to Register
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'New User? ',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: widget.registerFunc,
+                  child: Text(
+                    'Register Here!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- food_fete/lib/main.dart
+++ food_fete/lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home: LoginPage(),
+      home: LoginPage(
+        registerFunc: () {},
+      ),
     );
   }
 }
